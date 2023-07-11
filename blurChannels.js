1104f982cb38edc303d0b7a682ae8349e6f42a08
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-07-11 17:54:55 -0300

    blur channels

--- blurChannels.js
+++ blurChannels.js
@@ -1,17 +1,21 @@
-const form = document.querySelector('.form2');
-
-form.addEventListener('submit', async (event) => {
+const form1 = document.querySelector('.form2');
+console.log('using titles');
+form1.addEventListener('submit', async (event) => {
     event.preventDefault();
     const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
     chrome.scripting.executeScript({
         target: {tabId: tab.id},
-        function: blurChannels
+        function: blurChannel
     });
 });
 
-const blurChannels = () => {
-    const channels = document.querySelectorAll('.ytd-channel-name');
-    channels.forEach((channel) => channel.innerHTML = '************'
-    );
+const blurChannel = () => {
+    console.log('entrou');
+    
+    var titles = document.querySelectorAll(".yt-simple-endpoint.style-scope.yt-formatted-string");
+    for(var i=0; i<titles.length; i++){
+        titles[i].innerHTML = "********";
+    }
+
 }
