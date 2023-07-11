6613cc02beabf0d29da6f2952148ccbffa80cb79
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-07-11 17:09:16 -0300

    Update blurTitles.js

--- blurTitles.js
+++ blurTitles.js
@@ -12,7 +12,8 @@ form1.addEventListener('submit', async (event) => {
 
 const blurTitles = () => {
     console.log('entrou');
-    var titles = document.querySelectorAll(".style-scope.ytd-rich-grid-media");
+    
+    var titles = document.querySelectorAll(".yt-simple-endpoint.focus-on-expand.style-scope.ytd-rich-grid-media");
     for(var i=0; i<titles.length; i++){
         titles[i].innerHTML = "********";
     }
